369e3e81d2fa621b5559ea4099c7982e3daffbce
Anatolii Karachun <134794468+tolikkarachun@users.noreply.github.com>
2023-08-14 21:14:54 +0300

    commit

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -20,6 +20,21 @@ export class StatusComponent {
   title: "Current Transist: 3rd House",
   desc: "This is demonstrate siblings, hobbies, efforts, confidence, friends and short tr...",
 
+},
+{
+  id: 2,
+  icon: 'calendar_today',
+  title: "How was your day?",
+  desc: "Lorem ipsum dolor sit amet, consectetur adipscing elit. Ut vel odio en urna ultrice...",
+
+},
+{
+  id: 1,
+  icon: 'schedule',
+  title: "Current Transist: 3rd House",
+  desc: "This is demonstrate siblings, hobbies, efforts, confidence, friends and short tr...",
+
 }
+
 ]
 }
